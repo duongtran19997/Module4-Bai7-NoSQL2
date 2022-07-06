d0ceed89e49fe4df631e894644585ccbba52ed9b
superman <duonga2qp@gmail.com>
2022-07-06 09:08:36 +0700

    nop bai thuc hanh 2

---
+++ [Thuc-hanh]-Ung-dung-quan-ly-sach---Bo-sung-thong-tin-tac-gia/src/router/book.router.ts
@@ -0,0 +1,172 @@
+
+import { Router } from 'express';
+
+const bookRoutes = Router();
+
+import { Book } from "../schemas/book.model";
+
+import multer from 'multer';
+import {Author} from "../schemas/author.model";
+
+const upload = multer();
+
+
+bookRoutes.get('/create', (req, res) => {
+
+    res.render("createBook");
+
+});
+
+
+
+bookRoutes.post('/create', upload.none(), async (req, res) => {
+
+    try {
+        const authorNew = new Author({
+
+            name: req.body.author
+
+        })
+        await authorNew.save();
+
+
+        const bookNew = new Book({
+            title: req.body.title,
+            description: req.body.description,
+            author: authorNew._id,
+        });
+        bookNew.keywords.push({keyword: req.body.keyword});
+        console.log(bookNew);
+         await bookNew.save();
+        if (bookNew) {
+
+            res.render("success");
+
+        } else {
+
+            res.render("error");
+
+        }
+
+    } catch (err) {
+
+        res.render("error");
+
+    }
+
+});
+
+
+
+bookRoutes.post('/update', upload.none(), async (req, res) => {
+
+    try {
+
+        const book = await Book.findOne({ _id: req.body.id });
+
+        book.title = req.body.title;
+
+        book.description = req.body.description;
+
+        book.author = req.body.author;
+
+        await book.save();
+
+        if (book) {
+
+            res.render("success");
+
+        } else {
+
+            res.render("error");
+
+        }
+
+    } catch (err) {
+
+        res.render("error");
+
+    }
+
+});
+
+bookRoutes.get('/list', async (req, res) => {
+
+    try {
+
+
+        const books = await Book.find().populate({
+
+            path: "author", select: "name"
+
+        });
+        return res.json(books)
+        res.render("listBook", { books: books });
+
+    } catch {
+
+        res.render("error");
+
+    }
+
+});
+
+
+
+bookRoutes.get('/update/:id', async (req, res) => {
+
+    try {
+
+        const book = await Book.findOne({ _id: req.params.id });
+
+        console.log(book, 'book')
+
+        if (book) {
+
+            res.render("updateBook", {book: book})
+
+        } else {
+
+            res.render("error");
+
+        }
+
+    } catch (err) {
+
+        res.render("error");
+
+    }
+
+});
+
+
+
+bookRoutes.delete('/delete/:id', async (req, res) => {
+
+    try {
+
+        const book = await Book.findOne({ _id: req.params.id });
+
+        if (book) {
+
+            await book.remove();
+
+            res.status(200).json({ message: "success" });
+
+        } else {
+
+            res.render("error");
+
+        }
+
+    } catch (err) {
+
+        res.render("error");
+
+    }
+
+});
+
+
+
+export default bookRoutes;
